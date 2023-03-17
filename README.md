0663362aece1cbc2edad39b34cbb61af6a999976
Szymon Zyla <szymoon.zyla@gmail.com>
2023-03-17 16:09:44 +0100

    add txt/yaml example

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # py_many-species
 Simple and difficult solutions in Python language
+
+## version 0.1
+- add txt/yaml example
---
+++ txt/txt_yaml.py
@@ -0,0 +1,35 @@
+# YAML - Yet Another Markup Language
+# Load, change and save functions
+
+# https://pyyaml.org/wiki/PyYAMLDocumentation
+# pip install pyyaml
+import yaml
+
+
+def openYaml(n):
+    open("out_test.yaml", "w")
+
+    # test.yaml - k8s sample configuration
+    with open(r'test.yaml') as test_file:
+        # yaml.FullLoader - conversion from YAML to dictionary
+        i = 0
+        for data in yaml.load_all(test_file, Loader=yaml.FullLoader):
+            changeConfig(data)
+            saveYaml(data, i, n)
+            i += 1
+
+
+def changeConfig(data):
+    data['apiVersion'] = "newVersion1"
+
+
+def saveYaml(file, i, n):
+    with open(r'out_test.yaml', 'a') as out_file:
+        if i < n:
+            yaml.dump(file, out_file)
+            if i < n - 1:
+                out_file.write("---\n")
+
+
+# openYaml(number_of_documents)
+openYaml(3)
