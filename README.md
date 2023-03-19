4a0e9a64410ea66b1e76d832588c134901af86d2
Szymon Zyla <szymoon.zyla@gmail.com>
2023-03-19 20:54:45 +0100

    CircleCI, exceptions

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
-# py_many-species
+## py_many-species
 Simple and difficult solutions in Python language
 
-## version 0.1
+### version 0.1
 - add txt/yaml example
-## version 0.1.1
+### version 0.1.1
 - add txt/json example
+## version 0.1.2
+- CircleCI init config
+- txt/yaml - exceptions
+- txt/json - exceptions
--- txt/txt_yaml.py
+++ txt/txt_yaml.py
@@ -10,13 +10,18 @@ def openYaml(n):
     open("files/out_test.yaml", "w")
 
     # test.yaml - k8s sample configuration
-    with open(r'files/test.yaml') as test_file:
-        # yaml.FullLoader - conversion from YAML to dictionary
-        i = 0
-        for data in yaml.load_all(test_file, Loader=yaml.FullLoader):
-            changeConfig(data)
-            saveYaml(data, i, n)
-            i += 1
+    try:
+        with open(r'files/test.yaml') as test_file:
+            # yaml.FullLoader - conversion from YAML to dictionary
+            i = 0
+            for data in yaml.load_all(test_file, Loader=yaml.FullLoader):
+                changeConfig(data)
+                saveYaml(data, i, n)
+                i += 1
+    except FileNotFoundError:
+        print("err: open - file not found")
+    except TypeError:
+        print("err: open - yaml decode")
 
 
 def changeConfig(data):
@@ -24,11 +29,15 @@ def changeConfig(data):
 
 
 def saveYaml(file, i, n):
-    with open(r'files/out_test.yaml', 'a') as out_file:
-        if i < n:
-            yaml.dump(file, out_file)
-            if i < n - 1:
-                out_file.write("---\n")
+    try:
+        with open(r'files/out_test.yaml', 'a') as out_file:
+            if i < n:
+                yaml.dump(file, out_file)
+                print("ok: file saved")
+                if i < n - 1:
+                    out_file.write("---\n")
+    except FileNotFoundError:
+        print("err: save - file not found")
 
 
 # openYaml(number_of_documents)
