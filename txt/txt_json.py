0d8e94157a9db1ed7d4adaeb13280669e4b922e0
Szymon Zyla <szymoon.zyla@gmail.com>
2023-03-20 12:56:49 +0100

    first test

--- txt/txt_json.py
+++ txt/txt_json.py
@@ -1,14 +1,17 @@
 # JSON - JavaScript Object Notation
 # Load, change and save functions
 
+# python txt_yaml.py
+
 import json
 
 
 def openJson():
     try:
-        with open("files/test.json", encoding="UTF-8") as test_file:
+        with open("files/in.json", encoding="UTF-8") as test_file:
             data = json.load(test_file)
             changeConfig(data)
+            saveJson(data)
     except FileNotFoundError:
         print("err: open - file not found")
     except json.decoder.JSONDecodeError:
@@ -17,12 +20,12 @@ def openJson():
 
 def changeConfig(data):
     data["test1"]["value"] = "Bonjour le monde"
-    saveJson(data)
+    return data
 
 
 def saveJson(file):
     try:
-        with open("files/out_test.json", "w", encoding="UTF-8") as out_file:
+        with open("files/out.json", "w", encoding="UTF-8") as out_file:
             json.dump(file, out_file, ensure_ascii=False)
             print("ok: file saved")
     except FileNotFoundError:
